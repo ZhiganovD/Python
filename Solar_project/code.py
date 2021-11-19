1e85a4a14431a09aad3b7a4c25329755861239e3
ZhiganovD <91308641+ZhiganovD@users.noreply.github.com>
2021-11-19 09:56:34 +0300

    Update code.py

--- Solar_project/code.py
+++ Solar_project/code.py
@@ -1,1 +1,9 @@
-
+class HeavObj:
+    def __init__(self, x, y, Vx, Vy, radius, mass, color):
+        self.x = x
+        self.y = y
+        self.Vx = Vx
+        self.Vy = Vy
+        self.color = color
+        self.r = radius
+        self.m = mass
