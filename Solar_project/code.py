91d58c605e1a3ad06ea50a66129c0d7edf74a804
ZhiganovD <91308641+ZhiganovD@users.noreply.github.com>
2021-11-19 10:23:33 +0300

    Update code.py

--- Solar_project/code.py
+++ Solar_project/code.py
@@ -41,31 +41,41 @@ clock = pygame.time.Clock()
 finished = False
 finished0 = False
 finished1 = False
+screen.fill(WHITE)
+
+x1 = 260
+y1 = 300
+l1 = 200
+x2 = 250
+y2 = 370
+l2 = 100
+x3 = 270
+y3 = 440
+l3 = 150
+h = 30
+
+textsurface = myfont.render('Start game', False, (0, 0, 0))  #надпись в центре экрана
+screen.blit(textsurface,(x1, y1))
+textsurface1 = myfont.render('Leaderboard', False, (0, 0, 0))  #надпись в центре экрана
+screen.blit(textsurface1,(x2, y2))
+textsurface2 = myfont.render('Exit game', False, (0, 0, 0))  #надпись в центре экрана
+screen.blit(textsurface2,(x3, y3))
+pygame.display.update()
 
 while not finished0:
     clock.tick(FPS)
-    rect(screen, WHITE, (0, 0, a_sc, b_sc)) #заливаем экран белым
-    x1 = 260
-    y1 = 300
-    l1 = 200
-    x2 = 250
-    y2 = 370
-    l2 = 100
-    x3 = 270
-    y3 = 440
-    l3 = 150
-    h = 30
-    textsurface = myfont.render('Start game', False, (0, 0, 0))  #надпись в центре экрана
-    screen.blit(textsurface,(x1, y1))
-    textsurface1 = myfont.render('Leaderboard', False, (0, 0, 0))  #надпись в центре экрана
-    screen.blit(textsurface1,(x2, y2))
-    textsurface2 = myfont.render('Exit game', False, (0, 0, 0))  #надпись в центре экрана
-    screen.blit(textsurface2,(x3, y3))
-    pygame.display.update()
     for click in pygame.event.get():
         if click.type == pygame.MOUSEBUTTONDOWN: #нажата кнопка мыши
          if click.button == 1 and click.pos[0] < (x1 + l1) and click.pos[0] > (x1) and click.pos[1] < (y1 + h) and click.pos[1] > (y1 - h): 
              finished0 = True
+         if click.button == 1 and click.pos[0] < (x2 + l2) and click.pos[0] > (x2) and click.pos[1] < (y2 + h) and click.pos[1] > (y2 - h):
+             screen.fill(WHITE)
+             if click.type == pygame.MOUSEBUTTONDOWN: #нажата кнопка мыши
+                  if click.button == 3: #если нажал ПКМ
+                       sys.exit() #выключение программы
+             if click.type == pygame.QUIT:
+                   sys.exit() #выключение программы
+             pygame.display.update()
          if click.button == 1 and click.pos[0] < (x3 + l3) and click.pos[0] > (x3) and click.pos[1] < (y3 + h) and click.pos[1] > (y3 - h):
              sys.exit() #выключение программы
         if click.type == pygame.QUIT:
